9a83e45c268db6088110d8a140061bed1056e919
IamTy_ <iamtyv13@gmail.com>
2026-05-13 19:00:01 -0400

    Vistas/y/Parte/de/Modelo

--- src/models/Cl_mDonante.ts
+++ src/models/Cl_mDonante.ts
@@ -1,3 +1,42 @@
 // Cl_mDonante.ts
 
-export default class Cl_mDonante {}
+export default class Cl_mDonante {
+    private _nombre: string;
+    private _dolares: number;
+    private _bolivares: number;
+
+    constructor({nombre, dolares, bolivares}:
+        {nombre: string, dolares: number, bolivares: number} =
+        {nombre: "", dolares: 0, bolivares: 0}) {
+
+        this._nombre = nombre;
+        this._dolares = dolares;
+        this._bolivares = bolivares;
+    }   
+
+    set nombre(n: string) {
+        this._nombre = n;
+    }
+
+    get nombre(): string {
+        return this._nombre;
+    }
+
+    set dolares(d: number) {
+        this._dolares = d;
+    }
+
+    get dolares(): number {
+        return this._dolares;
+    }
+
+    set bolivares(b: number) {
+        this._bolivares = b;
+    }
+
+    get bolivares(): number {
+        return this._bolivares;
+    }
+
+    // Falta los Metodos de Transformacion de Bs a $ y de $ a Bs.
+}
--- src/views/Cl_vFundacionPlain.ts
+++ src/views/Cl_vFundacionPlain.ts
@@ -1,5 +1,77 @@
 // Cl_vFundacionPlain.ts
 
+import { I_vFundacion } from "../interfaces/I_vFundacion.js";
+import Cl_mDonante from "../models/Cl_mDonante.js";
+
+const html = String.raw; 
+/* 
+   String.ra2: Sirve para tomar el texto exactamente como lo escribes 
+(sin interpretar caracteres especiales como \n, \t, etc.) 
+*/
+
+export default class Cl_vFundacionPlain implements I_vFundacion {
+    private lblAporteTotalDolares: HTMLElement;
+    private lblAporteTotalBolivares: HTMLElement;
+    private lblTotalDolares: HTMLElement;
+    private lblTotalBolivares: HTMLElement;
+    private lblMejorDonador: HTMLElement;
+    private btNewDonante: HTMLButtonElement;
+    private tbDonantes: HTMLTableElement;
+    private vista: HTMLElement | null;
+
+    constructor() {
+        this.lblAporteTotalDolares = document.getElementById("body_lblAporteTotalDolares") as HTMLElement;
+        this.lblAporteTotalBolivares = document.getElementById("body_lblAporteTotalBolivares") as HTMLElement;
+        this.lblTotalDolares = document.getElementById("body_lblTotalDolares") as HTMLElement;
+        this.lblTotalBolivares = document.getElementById("body_lblTotalBolivares") as HTMLElement;
+        this.lblMejorDonador = document.getElementById("body_lblMejorDonador") as HTMLElement;
+        this.btNewDonante = document.getElementById("body_btNewDonante") as HTMLButtonElement;
+        this.tbDonantes = document.getElementById("body_tbDonantes") as HTMLTableElement;
+        this.vista = document.getElementById("body") as HTMLElement;
+    }
+
+    onNewDonante(callback: () => void): void {
+        this.btNewDonante.onclick = callback;      }
+
+    mostrarDonantes({
+            
+        donantes,
+        aporteTotalDolares, // Poner este metodo a la tabla
+        aporteTotalBolivares, // ""
+        totalDolares,
+        totalBolivares,
+        mejorDonador,
+
+    } : {
+
+        donantes: Cl_mDonante[];
+        aporteTotalDolares: number;
+        aporteTotalBolivares: number;
+        totalDolares: number;
+        totalBolivares: number;
+        mejorDonador: string;
+
+    }) : void {
+
+        this.tbDonantes.innerHTML = "";
+        donantes.forEach((donante) => {
+
+            const tr = document.createElement("tr");
+
+                tr.innerHTML = html`
+                    <td>${donante.nombre}</td>
+                    <td>${donante.dolares}</td>
+                    <td>${donante.bolivares}</td>
+                `;
+
+            this.tbDonantes.appendChild(tr);
+        });
+        this.lblTotalDolares.innerHTML = totalDolares.toString();
+        this.lblTotalBolivares.innerHTML = totalBolivares.toString();
+        this.lblMejorDonador.innerHTML = mejorDonador;
+    };
+}
+
 /*
     20. FUNDACIÓN VIEJITOS
 
